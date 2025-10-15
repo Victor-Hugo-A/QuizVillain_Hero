93bfd2e8be29aafb3af276e6789648fa804bf226
Victor-Hugo-A <victorpht.vh@gmail.com>
2025-10-15 15:01:34 -0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Quiz } from './components/quiz/quiz';
+import { Result } from './components/result/result';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Quiz }, // Rota inicial para o quiz
+  { path: 'result', component: Result }, // Rota para o resultado
+  { path: '**', redirectTo: '' } // Redireciona para o quiz em caso de rota desconhecida
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { App } from './app/app'; // Caso queira usar um componente principal
+import { routes } from './app/app.routes'; // Importa as rotas do arquivo de rotas
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideAnimations(),
+    provideRouter(routes), // Usando as rotas configuradas
+  ]
+}).catch(err => console.error(err));
